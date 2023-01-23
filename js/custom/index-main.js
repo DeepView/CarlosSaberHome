8dedf20eaca57093eac8d051dfe4a65584e36081
李华祥 <lihuaxiang0321@msn.cn>
2023-01-23 17:43:38 +0800

    create this project.

---
+++ js/custom/index-main.js
@@ -0,0 +1,12 @@
+function init() {
+    showFooter();
+}
+
+function showFooter() {
+    var year = new Date().getFullYear();
+    $('#footer-container').html('<p id="copyrights">&copy;' + year + ', Carlos</p>');
+}
+
+function go(url) {
+    window.location.href = url;
+}
